3e235fdac964967befeb5d711f6418b8fae80eed
HTWG-HeiHai <heettwei@htwg-konstanz.de>
2015-12-15 21:08:29 +0100

    websocket first try

--- play/GoBang_Web/app/controllers/Application.java
+++ play/GoBang_Web/app/controllers/Application.java
@@ -4,8 +4,12 @@ import views.html.*;
 import de.htwg.gobang.controller.IGbLogic;
 import de.htwg.gobang.game.GoBangGame;
 import de.htwg.gobang.ui.TUI;
+import de.htwg.gobang.observer.IObserver;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.WebSocket;
+import play.libs.F.Callback0;
+import play.libs.F.Callback;
 
 public class Application extends Controller {
 	
@@ -13,10 +17,6 @@ public class Application extends Controller {
 	IGbLogic controller;
 	TUI printer;
 	char lastAction = 'n';
-   
-    public Result test(String name) {
-    	return ok("Hello " + name);
-    }
     
     public Result game(){
     	games = GoBangGame.getInstance();
@@ -45,5 +45,46 @@ public class Application extends Controller {
     public Result checkField() {
     	return ok();
     }
-    
+
+    public void channel(WebSocket.Out<String> out, int x, int y, String color) {
+    	out.write(x + "_" + y + "-" + color);
+    }
+
+    public WebSocket<String> webSocket() {
+    	return new WebSocket<String>() {
+    		public void onReady(WebSocket.In<String> in, WebSocket.Out<String> out) {//mit json statt String!!
+    			new Watcher(controller);
+    			
+    			in.onMessage(new Callback<String>() {
+    				public void invoke(String field) {
+    					int x = Integer.parseInt(field.split("_")[0]) - 1;
+    					int y = Integer.parseInt(field.split("_")[1]) - 1;
+    					lastAction = controller.setToken(x, y);
+    					if(lastAction == 'e') {
+    						channel(out, x + 1, y + 1, controller.getcPlayer().getName());
+    					}
+    				}
+    			});
+    			in.onClose(new Callback0() {
+    				public void invoke() {
+    					
+    				}
+    			});
+    			out.write("message");
+    		}
+    	};
+    }
+
+	public class Watcher implements IObserver {
+		public Watcher(IGbLogic engine) {
+			engine.addObserver(this);
+		}
+
+		@Override
+		public void update() {
+//			field = controller.getField();
+//			channel(out)
+			System.out.println("heyyo");
+		}
+	}
 }
--- play/GoBang_Web/public/javascripts/app.js
+++ play/GoBang_Web/public/javascripts/app.js
@@ -1,24 +1,42 @@
 $(function() {
 	
-});
+	Server = new FancyWebSocket('ws://127.0.0.1:9000/websocket');
 
+	$('.btn-lg').click(function() {
+		checkField(this.id);
+	});
+	
+	Server.bind('message', function(setToken) {
+		field = setToken.split("-")[0];
+		color = setToken.split("-")[1];
+		updateField(field, color);
+	});
+
+	Server.connect();
+});
 
 var changeField = function(x, y) {
 	//alert(x + "," +y);
-	$.ajax({
-		type: "GET",
-		url: "/gobang/set/" + x + "/" + y,
-		data: {
-			'action': 'get',
-			'message': x + y
-		},
-		dataType: "json",
-		success: function() {
-			updateField(x + "_" + y);
-		},
-	});
+//	$.ajax({
+//		type: "GET",
+//		url: "/gobang/set/" + x + "/" + y,
+//		data: {
+//			'action': 'get',
+//			'message': x + y
+//		},
+//		dataType: "json",
+//		success: function() {
+//			updateField(x + "_" + y);
+//		},
+//	});
 };
 
-function updateField(source) {
-	$('#' + source).css('background-color', 'black');
+var Server;
+
+function checkField(source) {
+	Server.send('setField', source);
+};
+
+function updateField(source, color) {
+	$('#' + source).css('background-color', color);
 };
